1270a0fcf0cf1862af83f6c202674195af41a9d3
rathod-seema <100998142+rathod-seema@users.noreply.github.com>
2022-05-11 17:46:49 +0530

    Add files via upload

---
+++ ArrayListVectorTest.java
@@ -0,0 +1,14 @@
+package Day35;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayListVectorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TreeSetMain1.java
@@ -0,0 +1,22 @@
+package Day35;
+
+public class TreeSetMain1 {
+	
+	
+           public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			TreeSet<String> tset = new TreeSet<String>();
+			tset.add("dd");
+			tset.add("aaaa");
+			tset.add("cccc");
+			tset.add("bbb");
+			tset.add("duj");
+			System.out.println(tset);
+
+			
+
+		}
+
+	}
+
+
